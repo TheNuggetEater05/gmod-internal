76f95a718b55c4a0fb2218a331d33509bfda3c3f
letou <letou@DESKTOP-145CIAK>
2026-06-24 10:13:55 -0400

    Begin rewrite

--- sdk/valve/IClientBaseDLL.cpp
+++
@@ -1,9 +0,0 @@
-#include "IClientBaseDLL.h"
-
-#include "../../utility/Memory.h"
-
-ClientClass* IClientBaseDLL::GetAllClasses()
-{
-	using fn = ClientClass * (*)(void*);
-	return Memory::GetVFunc<fn>(this, 8)(this);
-}
